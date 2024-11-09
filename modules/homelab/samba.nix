e235af84e371f900fd4f0a1b94ebe1e27e020dd1
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-09 00:50:04 -0600

    [homelab] Add smb module

---
+++ modules/homelab/samba.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+{
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smb";
+        "netbios name" = "smb";
+        "security" = "user";
+        "hosts allow" = "192.168.12. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+    };
+      "nas" = {
+        "path" = "/nas";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "archvisions";
+      };
+    };
+  };
+ 
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+}
