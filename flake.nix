6162db79b585516bd0201f16558b75b6e542c80b
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-11 12:02:49 -0600

    [flake] Add sops-nix repo

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    inputs.sops-nix.url = "github:Mic92/sops-nix";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -32,7 +33,6 @@
           ./nixos/midnight/default.nix
         ];
       };
-
     };
   };
 }
