33aa3be0d48d95d7f74dc1d4f6188a07e07655b0
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-11 14:16:13 -0600

    [homelab] Allow Wireguard through Samba

--- modules/homelab/samba.nix
+++ modules/homelab/samba.nix
@@ -10,7 +10,7 @@
         "server string" = "smb";
         "netbios name" = "smb";
         "security" = "user";
-        "hosts allow" = "192.168.12. 127.0.0.1 localhost";
+        "hosts allow" = "192.168.12. 10.10.2. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
