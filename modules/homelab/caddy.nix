38acbf4b71064d2171ab866809ed458c21bf4c3a
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-11 14:13:49 -0600

    [homelab] Add Caddy configuration

--- modules/homelab/caddy.nix
+++ modules/homelab/caddy.nix
@@ -1,6 +1,23 @@
 { pkgs, config, ... }:
+
 {
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
   services.caddy = {
-  enable = true;
+  enable = true; # Working on configuration...
+      configFile = pkgs.writeText "Caddyfile" ''
+        (tls) {
+          tls /var/lib/caddy/archvisions.xyz/fullchain.cer /var/lib/caddy/archvisions.xyz/archvisions.xyz.key
+        }
+
+        music.archvisions.xyz {
+          import tls
+          reverse_proxy localhost:4533
+        }
+
+        nc.archvisions.xyz {
+          import tls
+          reverse_proxy localhost:8080
+        }
+    '';
   };
 }
