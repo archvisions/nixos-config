7f90bd1eec2f9ea714afc6fccb93266e612e3b46
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-12 17:17:40 -0600

    [homelab] Add the rest of minecraft.nix

--- modules/homelab/minecraft.nix
+++ modules/homelab/minecraft.nix
@@ -1,25 +1,23 @@
-{ pkgs, lib, inputs, ...}:
+{ pkgs, lib, inputs, ... }:
 
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "minecraft-server"
+  ];
   services.minecraft-servers = {
     enable = true;
     eula = true;
+    openFirewall = true;
     
     servers = {
       minecraft-minigame = {
         enable = true;
-        
-        packages = paperServers.paper-1.21.3;
 
-         symlinks = {
-           plugins = pkgs.linkFarmFromDrvs "plugins" (builtins.attrValues {
-           SkinsRestorer = fetchurl { url = "https://github.com/SkinsRestorer/SkinsRestorer/releases/download/15.4.4/SkinsRestorer.jar"; sha512 = ""; };
-             });
-           };
-         }
+        package = pkgs.paperServers.paper-1_21_3;
+
         serverProperties = {
           "max-players" = 4;
           "gamemode" = "survival";
@@ -29,9 +27,20 @@
           "motd" = "ArchVisions' Minigame server";
           "view-distance" = 10;
           "server-port" = 25565;
+          "ops" = "archvisions";
+        };
+
+        symlinks = {
+          "plugins/SkinsRestorer.jar" = pkgs.fetchurl rec {
+            pname = "SkinsRestorer";
+            version = "15.4.4";
+            url = "https://github.com/${pname}/${pname}/releases/download/${version}/${pname}.jar";
+            hash = "sha256-Dc/yZW5CsWU596+sGV5mWqOVeg4+QTNt5FM3yH7vsLw=";
+          };  
         };
 
         jvmOpts = "-Xms4096M -Xmx4096M";
       };
-   };
+    };
+  };
 }
