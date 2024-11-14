69a150f656ec4b8fc1662997a0c077078616cac5
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-14 13:49:51 -0600

    [flake] Clean up

--- flake.nix
+++ flake.nix
@@ -2,22 +2,35 @@
   description = "nixos-config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
-    catppuccin.url = "github:catppuccin/nix";
+
+    nixpkgs = { 
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    home-manager = { 
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-minecraft = {
+      url = "github:Infinidoge/nix-minecraft";
+    };
+
+    catppuccin.url = {
+      "github:catppuccin/nix";
+    };
+
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, sops-nix, ... }: {
-    nixosConfigurations =
-      let
-        system = "x86_64-linux";
-      in
-      {
-      
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+
+    nixosConfigurations = {
+
       zion = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
