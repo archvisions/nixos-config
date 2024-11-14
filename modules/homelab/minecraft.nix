1fea0df5ccf71dac70bf3411593e9c7b4a2ab79a
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-14 17:08:54 -0600

    [homelab] Add more to minecraft.nix

--- modules/homelab/minecraft.nix
+++ modules/homelab/minecraft.nix
@@ -4,9 +4,6 @@
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "minecraft-server"
-  ];
   services.minecraft-servers = {
     enable = true;
     eula = true;
@@ -14,6 +11,7 @@
     
     servers = {
       minecraft-minigame = {
+        autoStart = false;
         enable = true;
 
         package = pkgs.paperServers.paper-1_21_3;
@@ -22,12 +20,11 @@
           "max-players" = 4;
           "gamemode" = "survival";
           "online-mode" = false;
-          "enable-command-block" = false;
+          "enable-command-block" = true;
           "log-ips" = false;
           "motd" = "ArchVisions' Minigame server";
           "view-distance" = 10;
           "server-port" = 25565;
-          "ops" = "archvisions";
         };
 
         symlinks = {
@@ -39,6 +36,33 @@
           };  
         };
 
+        jvmOpts = "-Xms4096M -Xmx4096M";
+      };
+      archvisions-smp = {
+        enable = true;
+        autoStart = false;
+
+        package = pkgs.paperServers.paper-1_21_3;
+
+        serverProperties = {
+          "max-players" = 4;
+          "gamemode" = "survival";
+          "online-mode" = false;
+          "enable-command-block" = false;
+          "log-ips" = false;
+          "motd" = "ArchVisions' SMP";
+          "view-distance" = 10;
+          "server-port" = 25566;
+        };
+
+        symlinks = {
+          "plugins/SkinsRestorer.jar" = pkgs.fetchurl rec {
+            pname = "SkinsRestorer";
+            version = "15.4.4";
+            url = "https://github.com/${pname}/${pname}/releases/download/${version}/${pname}.jar";
+            hash = "sha256-Dc/yZW5CsWU596+sGV5mWqOVeg4+QTNt5FM3yH7vsLw=";
+          };  
+        };
         jvmOpts = "-Xms4096M -Xmx4096M";
       };
     };
