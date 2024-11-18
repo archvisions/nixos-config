e491dcea76e9b4fc47c3081be41dd999d2639b19
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-17 19:42:41 -0600

    [home-manager] Add more to ZSH

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -4,15 +4,25 @@
 
   programs.zsh = {
     enable = true; 
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git"];
+      theme = "gozilla";
+    };
 
     history = {
       size = 10000;
     };
-
+ 
     shellAliases = {
       ns = "sudo nixos-rebuild switch --flake ~/nixos-config";
       rate = "xrandr --output HDMI-0 --mode 1920x1080 --rate 75";
       server = "ssh archvisions@192.168.12.117";
+
     };
   };
 }
