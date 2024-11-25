c8fa55e040111f1b64ab40e90eaf1d5d0ead8f78
archvisions <184866510+archvisions@users.noreply.github.com>
2024-11-25 13:21:57 -0600

    [homelab] Add invidious setting

--- modules/homelab/invidious.nix
+++ modules/homelab/invidious.nix
@@ -13,6 +13,7 @@
         player_style = "youtube";
         quality = "dash";
         dark_mode = "dark";
+        quality_dash = "1080p";
       };
       port = lib.mkDefault 3000;
       signature_server = lib.mkDefault "127.0.0.1:2999";
@@ -20,6 +21,7 @@
       po_token = "Mnhv3F7b8GDtDm_gY4adeKxI-sxBsmRWa266q7IibYK7Eziw6Z7Z3VCVRWfIaj3PM4ZhQw4jt5KR2erA5sRKt1MZCuoIGb_nuIClkERxG9CPOEBLs9dimwh2KY33qPlnrGL8HsHgP7s40OtgegM9lPHgPryAGd0j5WM=";
       visitor_data = "CgtYQmNIY2VwLVNWSSiov5K6BjIKCgJVUxIEGgAgRQ%3D%3D";
       https = true;
+      
     };
     database = {
       createLocally = true;
